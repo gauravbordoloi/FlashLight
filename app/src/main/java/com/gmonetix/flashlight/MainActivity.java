25622a43aeea5291cdcc5d400c3af1b977351e4c
Gmonetix <gmonetix@gmail.com>
2016-12-15 18:51:06 -0800

    Initial commit

---
+++ app/src/main/java/com/gmonetix/flashlight/MainActivity.java
@@ -0,0 +1,157 @@
+package com.gmonetix.flashlight;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnSwitch;
+    private Camera camera;
+    private boolean isFlashOn , hasFlash;
+    Parameters params;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // flash power switch button
+        btnSwitch = (Button) findViewById(R.id.power);
+
+        // First check if device is supporting flashlight or not
+        hasFlash = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+        if (!hasFlash) {
+            // device doesn't support flash
+            // Show alert message and close the application
+            AlertDialog alert = new AlertDialog.Builder(MainActivity.this).create();
+            alert.setTitle("Error");
+            alert.setMessage("Sorry, your device doesn't support flash light!");
+            alert.setButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // closing the application
+                    finish();
+                }
+            });
+            alert.show();
+            return;
+        }
+
+        // get the camera
+        getCamera();
+
+        // displaying button Text from Strings.xml
+        toggleButtonText();
+
+        // Switch button click event to toggle flash on/off
+        btnSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isFlashOn) {
+                    // turn off flash
+                    turnOffFlash();
+                } else {
+                    // turn on flash
+                    turnOnFlash();
+                }
+            }
+        });
+    }
+
+    // Get the camera
+    private void getCamera() {
+        if (camera == null) {
+            try {
+                camera = Camera.open();
+                params = camera.getParameters();
+            } catch (RuntimeException e) {
+                //Log.e("Cao Open. Error: ", e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Turning On flash
+    private void turnOnFlash() {
+        if (!isFlashOn) {
+            if (camera == null || params == null) {
+                return;
+            }
+            params = camera.getParameters();
+            params.setFlashMode(Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(params);
+            camera.startPreview();
+            isFlashOn = true;
+            toggleButtonText(); // changing button switch text
+        }
+    }
+
+    // Turning Off flash
+    private void turnOffFlash() {
+        if (isFlashOn) {
+            if (camera == null || params == null) {
+                return;
+            }
+            params = camera.getParameters();
+            params.setFlashMode(Parameters.FLASH_MODE_OFF);
+            camera.setParameters(params);
+            camera.stopPreview();
+            isFlashOn = false;
+            toggleButtonText(); // changing button switch text
+        }
+    }
+
+    // Function to toggle the text in button from strings.xml
+    private void toggleButtonText() {
+        if (isFlashOn) {
+            btnSwitch.setText(R.string.power_off);
+        } else {
+            btnSwitch.setText(R.string.power_on);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        turnOffFlash(); // on pause turn off the flash
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (hasFlash)
+            turnOnFlash();  // on resume turn on the flash
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getCamera();    // on starting the app get the camera params
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // on stop release the camera
+        if (camera != null) {
+            camera.release();
+            camera = null;
+        }
+    }
+}
